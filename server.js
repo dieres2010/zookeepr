6c06bf1288a8d189b877b019502f0a68ec30a5d0
dieres <dieres2010@gmail.com>
2022-03-02 20:11:13 -0500

    Environment Variable PORT set

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 // server instantiation
 const app = express();
 
@@ -53,6 +55,6 @@ app.get('/api/animals', (req, res) => {
 
 
 // chain the listen method to server
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
